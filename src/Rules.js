c4af345ff61130bd0dd56f508e6f11e7c021dfde
Jared Evans <jarede@gmail.com>
2026-01-22 00:03:19 +0000

    update Rules to run JSONPath checks

--- src/Rules.js
+++ src/Rules.js
@@ -1,5 +1,38 @@
 "use strict";
 
+/**
+ * @typedef {Object} criteriaType
+ * @property {('simple'|'regex'|'jsonpath'|'xpath')} type - REQUIRED. The type of condition to be applied. The options allowed are jsonpath or xpath.
+ * @property {string=} version - REQUIRED. A short hand string representing the version of the expression type being used. The allowed values for JSONPath are draft-goessner-dispatch-jsonpath-00. The allowed values for XPath are xpath-30, xpath-20, or xpath-10.
+ */
+
+/**
+ * @typedef {Object} criteria
+ * @property {string} context - A Runtime Expression used to set the context for the condition to be applied on. If type is specified, then the context MUST be provided (e.g. $response.body would set the context that a JSONPath query expression could be applied to).
+ * @property {string} condition - REQUIRED. The condition to apply. Conditions can be simple (e.g. $statusCode == 200 which applies an operator on a value obtained from a runtime expression), or a regex, or a JSONPath expression. For regex or JSONPath, the type and context MUST be specified.
+ * @property {criteriaType} type - The type of condition to be applied. If specified, the options allowed are simple, regex, jsonpath or xpath. If omitted, then the condition is assumed to be simple, which at most combines literals, operators and Runtime Expressions. If jsonpath, then the expression MUST conform to JSONPath. If xpath the expression MUST conform to XML Path Language 3.1. Should other variants of JSONPath or XPath be required, then a Criterion Expression Type Object MUST be specified.
+ */
+
+/**
+ * @typedef {Object} failureActionsObject
+ * @property {string} name - REQUIRED. The name of the failure action. Names are case sensitive.
+ * @property {string} type - REQUIRED. The type of action to take. Possible values are "end", "retry", or "goto".
+ * @property {string=} workflowId - The workflowId referencing an existing workflow within the Arazzo Description to transfer to upon failure of the step. This field is only relevant when the type field value is "goto" or "retry". If the referenced workflow is contained within an arazzo type sourceDescription, then the workflowId MUST be specified using a Runtime Expression (e.g., $sourceDescriptions.<name>.<workflowId>) to avoid ambiguity or potential clashes. This field is mutually exclusive to stepId. When used with "retry", context transfers back upon completion of the specified workflow.
+ * @property {string=} stepId - The stepId to transfer to upon failure of the step. This field is only relevant when the type field value is "goto" or "retry". The referenced stepId MUST be within the current workflow. This field is mutually exclusive to workflowId. When used with "retry", context transfers back upon completion of the specified step.
+ * @property {number=} retryAfter - A non-negative decimal indicating the seconds to delay after the step failure before another attempt SHALL be made. Note: if an HTTP Retry-After response header was returned to a step from a targeted operation, then it SHOULD overrule this particular field value. This field only applies when the type field value is "retry".
+ * @property {number=} retryLimit - A non-negative integer indicating how many attempts to retry the step MAY be attempted before failing the overall step. If not specified then a single retry SHALL be attempted. This field only applies when the type field value is "retry". The retryLimit MUST be exhausted prior to executing subsequent failure actions.
+ * @property {criteria[]} criteria - A list of assertions to determine if this action SHALL be executed. Each assertion is described using a Criterion Object.
+ */
+
+/**
+ * @typedef {Object} successActionsObject
+ * @property {string} name - REQUIRED. The name of the failure action. Names are case sensitive.
+ * @property {string} type - REQUIRED. The type of action to take. Possible values are "end" or "goto".
+ * @property {string=} workflowId -  The workflowId referencing an existing workflow within the Arazzo Description to transfer to upon success of the step. This field is only relevant when the type field value is "goto". If the referenced workflow is contained within an arazzo type sourceDescription, then the workflowId MUST be specified using a Runtime Expression (e.g., $sourceDescriptions.<name>.<workflowId>) to avoid ambiguity or potential clashes. This field is mutually exclusive to stepId.
+ * @property {string=} stepId - The stepId to transfer to upon success of the step. This field is only relevant when the type field value is "goto". The referenced stepId MUST be within the current workflow. This field is mutually exclusive to workflowId.
+ * @property {criteria[]} criteria - A list of assertions to determine if this action SHALL be executed. Each assertion is described using a Criterion Object. All criteria assertions MUST be satisfied for the action to be executed.
+ */
+
 class Rules {
   constructor(expression, options) {
     this.expression = expression;
@@ -12,39 +45,6 @@ class Rules {
       success: () => {},
     };
   }
-
-  /**
-   * @typedef {Object} criteriaType
-   * @property {string} type - REQUIRED. The type of condition to be applied. The options allowed are jsonpath or xpath.
-   * @property {string=} version - REQUIRED. A short hand string representing the version of the expression type being used. The allowed values for JSONPath are draft-goessner-dispatch-jsonpath-00. The allowed values for XPath are xpath-30, xpath-20, or xpath-10.
-   */
-
-  /**
-   * @typedef {Object} criteria
-   * @property {string} context - A Runtime Expression used to set the context for the condition to be applied on. If type is specified, then the context MUST be provided (e.g. $response.body would set the context that a JSONPath query expression could be applied to).
-   * @property {string} condition - REQUIRED. The condition to apply. Conditions can be simple (e.g. $statusCode == 200 which applies an operator on a value obtained from a runtime expression), or a regex, or a JSONPath expression. For regex or JSONPath, the type and context MUST be specified.
-   * @property {criteriaType} type - The type of condition to be applied. If specified, the options allowed are simple, regex, jsonpath or xpath. If omitted, then the condition is assumed to be simple, which at most combines literals, operators and Runtime Expressions. If jsonpath, then the expression MUST conform to JSONPath. If xpath the expression MUST conform to XML Path Language 3.1. Should other variants of JSONPath or XPath be required, then a Criterion Expression Type Object MUST be specified.
-   */
-
-  /**
-   * @typedef {Object} failureActionsObject
-   * @property {string} name - REQUIRED. The name of the failure action. Names are case sensitive.
-   * @property {string} type - REQUIRED. The type of action to take. Possible values are "end", "retry", or "goto".
-   * @property {string=} workflowId - The workflowId referencing an existing workflow within the Arazzo Description to transfer to upon failure of the step. This field is only relevant when the type field value is "goto" or "retry". If the referenced workflow is contained within an arazzo type sourceDescription, then the workflowId MUST be specified using a Runtime Expression (e.g., $sourceDescriptions.<name>.<workflowId>) to avoid ambiguity or potential clashes. This field is mutually exclusive to stepId. When used with "retry", context transfers back upon completion of the specified workflow.
-   * @property {string=} stepId - The stepId to transfer to upon failure of the step. This field is only relevant when the type field value is "goto" or "retry". The referenced stepId MUST be within the current workflow. This field is mutually exclusive to workflowId. When used with "retry", context transfers back upon completion of the specified step.
-   * @property {number=} retryAfter - A non-negative decimal indicating the seconds to delay after the step failure before another attempt SHALL be made. Note: if an HTTP Retry-After response header was returned to a step from a targeted operation, then it SHOULD overrule this particular field value. This field only applies when the type field value is "retry".
-   * @property {number=} retryLimit - A non-negative integer indicating how many attempts to retry the step MAY be attempted before failing the overall step. If not specified then a single retry SHALL be attempted. This field only applies when the type field value is "retry". The retryLimit MUST be exhausted prior to executing subsequent failure actions.
-   * @property {criteria[]} criteria - A list of assertions to determine if this action SHALL be executed. Each assertion is described using a Criterion Object.
-   */
-
-  /**
-   * @typedef {Object} successActionsObject
-   * @property {string} name - REQUIRED. The name of the failure action. Names are case sensitive.
-   * @property {string} type - REQUIRED. The type of action to take. Possible values are "end" or "goto".
-   * @property {string=} workflowId -  The workflowId referencing an existing workflow within the Arazzo Description to transfer to upon success of the step. This field is only relevant when the type field value is "goto". If the referenced workflow is contained within an arazzo type sourceDescription, then the workflowId MUST be specified using a Runtime Expression (e.g., $sourceDescriptions.<name>.<workflowId>) to avoid ambiguity or potential clashes. This field is mutually exclusive to stepId.
-   * @property {string=} stepId - The stepId to transfer to upon success of the step. This field is only relevant when the type field value is "goto". The referenced stepId MUST be within the current workflow. This field is mutually exclusive to workflowId.
-   * @property {criteria[]} criteria - A list of assertions to determine if this action SHALL be executed. Each assertion is described using a Criterion Object. All criteria assertions MUST be satisfied for the action to be executed.
-   */
 
   /**
    * Sets the Failure Action Rules to be run
@@ -158,27 +158,69 @@ class Rules {
               `${criteriaObject.context} did not match ${criteriaObject.condition}`,
             );
           }
-          // if (hasPassedCheck) hasPassed.push(true);
-        } else {
+        } else if (criteriaObject.type === "jsonpath") {
+          const hasPassedCheck = this.jsonPathCheck(rule, criteriaObject);
+          if (hasPassedCheck) passedCriteria.push(hasPassedCheck);
+        } else if (criteriaObject.type === "simple") {
+          const hasPassedCheck = this.simpleCheck(rule, criteriaObject);
+          if (hasPassedCheck) passedCriteria.push(hasPassedCheck);
         }
       } else {
-        this.logger.notice(
-          `checking ${rule.name} rule: ${criteriaObject.condition} `,
-        );
-        const hasPassedCheck = this.expression.checkSimpleExpression(
-          criteriaObject.condition,
-        );
-
-        if (hasPassedCheck) {
-          this.logger.success(`${criteriaObject.condition} passed`);
-          passedCriteria.push(hasPassedCheck);
-        } else {
-          this.logger.error(`${criteriaObject.condition} failed`);
-        }
+        const hasPassedCheck = this.simpleCheck(rule, criteriaObject);
+        if (hasPassedCheck) passedCriteria.push(hasPassedCheck);
       }
     }
 
     return passedCriteria;
+  }
+
+  /**
+   * @function simpleCheck
+   * @private
+   * @param {successActionsObject|failureActionsObject} rule
+   * @param {criteriaObject} criteriaObject
+   * @returns {boolean}
+   */
+  simpleCheck(rule, criteriaObject) {
+    this.logger.notice(
+      `checking ${rule.name} rule: ${criteriaObject.condition} `,
+    );
+    const hasPassedCheck = this.expression.checkSimpleExpression(
+      criteriaObject.condition,
+    );
+
+    if (hasPassedCheck) {
+      this.logger.success(`${criteriaObject.condition} passed`);
+    } else {
+      this.logger.error(`${criteriaObject.condition} failed`);
+    }
+
+    return hasPassedCheck;
+  }
+
+  /**
+   * @function jsonPathCheck
+   * @private
+   * @param {successActionsObject|failureActionsObject} rule
+   * @param {criteriaObject} criteriaObject
+   * @returns {boolean}
+   */
+  jsonPathCheck(rule, criteriaObject) {
+    this.logger.notice(
+      `checking ${rule.name} rule: ${criteriaObject.context} has a matching JSONPath for ${criteriaObject.condition} `,
+    );
+    const hasPassedCheck = this.expression.checkJSONPathExpression(
+      criteriaObject.context,
+      criteriaObject.condition,
+    );
+
+    if (hasPassedCheck) {
+      this.logger.success(`${criteriaObject.condition} passed`);
+    } else {
+      this.logger.error(`${criteriaObject.condition} failed`);
+    }
+
+    return hasPassedCheck;
   }
 
   /**
